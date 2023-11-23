60fc4dad6f750d0a3be74cad065eef58f8f4bde4
Mkson-Assis <marcksonasantos@gmail.com>
2023-11-23 18:33:26 -0300

    First commit

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,24 @@
+<h1>Listagem dos supports</h1>
+
+{{ $xss }}
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th>Ações</th>
+    </thead>
+    
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>></td>
+            </tr>
+        @endforeach
+    </tbody>
+
+</table>
